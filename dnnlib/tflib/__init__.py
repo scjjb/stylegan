b091bede36328c32630f40fcb395f9fe5c0ad5e9
Jack Breen <77981425+scjjb@users.noreply.github.com>
2021-07-13 16:34:43 +0100

    Update __init__.py

--- dnnlib/tflib/__init__.py
+++ dnnlib/tflib/__init__.py
@@ -5,12 +5,12 @@
 # http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
 # Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
-from stylegan.dnnlib import autosummary
-from stylegan.dnnlib import network
-from stylegan.dnnlib import optimizer
-from stylegan.dnnlib import tfutil
+from stylegan.dnnlib.tflib import autosummary
+from stylegan.dnnlib.tflib import network
+from stylegan.dnnlib.tflib import optimizer
+from stylegan.dnnlib.tflib import tfutil
 
-from stylegan.dnnlib.tfutil import *
-from stylegan.dnnlib.network import Network
+from stylegan.dnnlib.tflib.tfutil import *
+from stylegan.dnnlib.tflib.network import Network
 
-from stylegan.dnnlib.optimizer import Optimizer
+from stylegan.dnnlib.tflib.optimizer import Optimizer
